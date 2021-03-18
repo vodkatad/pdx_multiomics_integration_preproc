9a7c0ca96ecd2cf60da28333424857e5481c77b3
Umberto Perron <umberto.perron@fht.org>
2021-03-18 13:39:58 +0100

    fix reading filenames from snakemake object

--- local/src/DIABLO_integrated_model.R
+++ local/src/DIABLO_integrated_model.R
@@ -7,29 +7,33 @@ f <- snakemake@input[["expr_Xtrain"]]
 expr <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(expr)<-expr$V1
 expr<-subset(expr, select = -c(V1))
-f <- "tables/PDx_Meth_MultiFeatSelect_XtrainClean.tsv"
+print("loaded expr")
 f <- snakemake@input[["meth_Xtrain"]]
 meth <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(meth)<-rownames(expr)
 meth<-subset(meth, select = -c(V1))
+print("loaded meth")
 f <- snakemake@input[["cnv_Xtrain"]]
 cnv <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(cnv)<-rownames(expr)
 cnv<-subset(cnv, select = -c(V1))
+print("loaded cnv")
 f <- snakemake@input[["mut_Xtrain"]]
 mut <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(mut)<-rownames(expr)
 mut<-subset(mut, select = -c(V1))
-
+print("loaded mut")
 # load target
 f <- snakemake@input[["Ytrain"]]
 Y<-as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(Y)<-rownames(expr)
-Y<-factor(Y$snakemake@params[["target_col"]])
+target_col<-quote(snakemake@params[["target_col"]])
+Y<-factor(Y[,eval(target_col)])
+print("loaded Y")
 
 # integrate the omics
 data<-list(expr=expr, mut=mut, meth=meth, cnv=cnv)
-
+print("listed data")
 #first fit a DIABLO model without variable selection to assess 
 # the global performance and choose the number of components for the final model. 
 
@@ -39,22 +43,29 @@ design=matrix(1,ncol=length(data),
               nrow=length(data),
               dimnames=list(names(data),names(data)))
 diag(design)=0
-
+print("build design matrix")
 # assess the performance of this first model
 # perf is run with nfold-fold cross validation for nrepeat times. 
 splsda.res = block.splsda(X = data, Y = Y, ncomp = 8, design = design)
+print("set model")
 perf.diablo = perf(splsda.res,
                    validation = 'Mfold', 
                    folds = 2, 
                    nrepeat = 5, 
                    progressBar=FALSE,
                    cpus=snakemake@threads)
+print("check performance")
 # plot error rate by number of components used by the model
-pdf(snakemake@output[["ncompBER_plot"]]
-    width = 8, height = 5, bg = "white", 
-    colormodel = "cmyk",paper = "A4") 
+f <- snakemake@output[["ncompBER_plot"]]
+pdf(f,
+    width = 8,
+    height = 5,
+    bg = "white", 
+    colormodel = "cmyk",
+    paper = "A4") 
 plot(perf.diablo) 
 dev.off() 
+print("plot ncompBER")
 
 # tune the model on the train set
 # optimise the number of features for each 'omic, component
@@ -62,10 +73,10 @@ test.keepX=list("expr"=c(seq(5, 30, 10)),
                 "mut"=c(seq(5, ncol(mut), 5)),
                "meth"=c(seq(5, 30, 10)),
                 "cnv"=c(seq(5, 30, 10)))
-optimal_ncomp<-snakemake@params[["max_ncomp"]]
+max_ncomp<-snakemake@params[["max_ncomp"]]
 tune.omics=tune.block.splsda(X=data,
                              Y=Y,
-                             ncomp=optimal_ncomp,
+                             ncomp=max_ncomp,
                              test.keepX=test.keepX,
                              design=design,
                              cpus=snakemake@threads,
@@ -75,7 +86,7 @@ tune.omics=tune.block.splsda(X=data,
                              nrepeat=50,
                              near.zero.var=FALSE,
                              dist = "mahalanobis.dist")
-
+print("tuned the model")
 # train final sPLS-DA model,
 # use optimal number of components, features
 optimal_ncomp<-tune.omics$choice.ncomp$ncomp 
@@ -89,27 +100,36 @@ res=block.splsda(X=data,
                  keepX=list.keepX,
                  design=design,
                  near.zero.var=FALSE)
+print("built final model")
+
 # save trained model as RDS file
-saveRDS(res, file = snakemake@output[["splsda_model"]])
+f <- snakemake@output[["splsda_model"]] 
+saveRDS(res, file = f)
+print("saved model")
 
 # assess performance on train set (4-fold CV x 100 repeats) 
 # with Weighted prediction
 perf.res <- perf(res, validation = "Mfold", 
                  folds = 4,  nrepeat = 100,
                  dist = "mahalanobis.dist") 
+print("check model perf on training set")
+
 # log balanced error rate on training set
 perf_res_train<-as.data.frame(perf.res$WeightedVote.error.rate)
+f <- snakemake@output[["trainBER_table"]]
 write.table(perf_res_train, 
-            file = snakemake@output[["trainBER_table"]],
+            file = f,
             quote = FALSE, 
             sep = "\t",
             row.names = TRUE,
             col.names = TRUE)
+print("save train BER")
 
 # Low-dimensional latent PLS space representation of each individual OMIC
 # samples are represented as points placed according to 
 # their projection in the smaller subspace spanned by the components
-pdf(snakemake@output[["PlotIndiv"]],
+f <- snakemake@output[["PlotIndiv"]]
+pdf(f,
     width = 10, height = 6, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
 plotIndiv(res,legend=TRUE,
@@ -118,9 +138,11 @@ plotIndiv(res,legend=TRUE,
           ind.names=FALSE,
           cex=.5)
 dev.off() 
+print("plot indiv")
 
 # show and save loadings for each omic and each component
-pdf(snakemake@output[["PlotLoadings"]],
+f<-snakemake@output[["PlotLoadings"]]
+pdf(f,
     width = 10, height = 6, bg = "white", 
     colormodel = "cmyk",paper = "A4")
 for (c in seq(0, optimal_ncomp))
@@ -128,21 +150,25 @@ for (c in seq(0, optimal_ncomp))
     plotLoadings(res,comp=c,contrib='max',method='median')
 }
 dev.off()
+print("plot loadings")
 loadings<-as.data.frame(plotLoadings(res,
                              contrib='max',
                              method='median',
-                             plot=FALSE)) 
+                             plot=FALSE))
+f<-snakemake@output[["loadings_table"]]
 write.table(loadings, 
-            file = snakemake@output[["loadings_table"]],
+            file = f,
             quote = FALSE,
             sep = "\t",
             row.names = TRUE,
             col.names = TRUE)
+print("write loadings")
 
 # Now we will diplay each individual on the plot of each of the OMICs
 # against each other which are defined by the top loadings from 
 # their respective components.
-pdf(snakemake@output[["PlotLoadings"]],
+f<-snakemake@output[["plotDiablo"]]
+pdf(f,
     width = 10, height = 6, bg = "white", 
     colormodel = "cmyk",paper = "A4")
 for (c in seq(0, optimal_ncomp))
@@ -150,6 +176,7 @@ for (c in seq(0, optimal_ncomp))
    plotDiablo(res,ncomp=c) 
 }
 dev.off()
+print("plot Diablo")
 
 # Now let us display so-called “arrow plot”  
 # The start of the arrow indicates the location of the sample in X in one plot, 
@@ -158,18 +185,21 @@ dev.off()
 # long arrows a disagreement between the matching data sets.
 # This plot highlights the agreement between all data sets at 
 # the sample level, when modelled with DIABLO.
-pdf(snakemake@output[["plotArrow"]],
+f<-snakemake@output[["plotArrow"]]
+pdf(f,
     width = 8, height = 6, bg = "white",
     colormodel = "cmyk",paper = "A4") 
 plotArrow(res,ind.names=FALSE,legend=TRUE,
           title="PDx omics integration arrow plot")
 dev.off() 
+print("plot arrow")
 
 # calculate “circos plot” that diaplays variable 
 # correlation among different OMICs datasets
 # the variables for this plot were selected simultaneously from all the OMICs, i.e. they are 
 # not equavalent to those obtained from each individual OMIC separately.
-pdf(snakemake@output[["circosPlot"]],
+f<-snakemake@output[["circosPlot"]]
+pdf(f,
     width = 18, height = 16, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
 circosPlot(res,
@@ -177,15 +207,18 @@ circosPlot(res,
            line=FALSE,
            size.variables=0.5)
 dev.off()
+print("plot circos")
 
 # Plot the correlation heatmap showing strongly correlated blocks 
 # of gene expression, methylation, mutation, adn CNV 
 # that provide clustering of individuals into females and males.
-pdf(snakemake@output[["corrHeatmap"]]
+f<-snakemake@output[["corrHeatmap"]]
+pdf(f,
     width = 24, height = 14, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
 cimDiablo(res)
 dev.off()
+print("plot heatmap")
 
 ### Predict target classes from OMICs Integration,
 # assess prediction accuracy
@@ -212,7 +245,8 @@ f <- snakemake@input[["Ytest"]]
 Y_test<-as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(Y_test)<-rownames(expr_test)
 Y_test<-subset(Y_test, select = -c(V1))
-Y_test<-factor(Y_test$snakemake@params[["target_col"]])
+Y_test<-factor(Y_test[, eval(target_col)])
+print("load testfiles")
 
 # build test set
 data.test<-list(expr=expr_test,mut=mut_test,meth=meth_test,cnv=cnv_test)
@@ -222,24 +256,28 @@ lapply(data.test, dim)
 predict.diablo=predict(res,
                        newdata=data.test,
                        dist='mahalanobis.dist')
+print("make predictions")
 
 predictions<-predict.diablo$WeightedVote$mahalanobis.dist[,optimal_ncomp]
 confusion.mat = get.confusion_matrix(truth = Y_test, 
                                      predicted = predictions)
 # save confusion matrix
+f<-snakemake@output[["confusionMat"]]
 write.table(confusion.mat, 
-            file = snakemake@output[["confusionMat"]],
+            file = f,
             quote = FALSE,
             sep = "\t",
             row.names = TRUE,
             col.names = TRUE) 
+print("write confusion mat")
 
 for (c in seq(0, optimal_ncomp))
 {
 # save balanced error rate for e/a component
+f<-snakemake@log[[1]]
 lapply(c(c, get.BER(confusion.mat)), 
          write,
-         snakemake@log[[1]], 
+         f, 
          append=TRUE)
 }
 
