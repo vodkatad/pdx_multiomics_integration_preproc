60b104ea03aa4ae341b18c345e8bf06ad88cbb31
Umberto Perron <umberto.perron@fht.org>
2021-03-18 11:47:15 +0100

    move to snakemake script

--- local/src/DIABLO_integrated_model.R
+++ local/src/DIABLO_integrated_model.R
@@ -43,9 +43,12 @@ diag(design)=0
 # assess the performance of this first model
 # perf is run with nfold-fold cross validation for nrepeat times. 
 splsda.res = block.splsda(X = data, Y = Y, ncomp = 8, design = design)
-perf.diablo = perf(splsda.res, validation = 'Mfold', 
-                   folds = 2, nrepeat = 5, 
-                   progressBar=FALSE, cpus=4)
+perf.diablo = perf(splsda.res,
+                   validation = 'Mfold', 
+                   folds = 2, 
+                   nrepeat = 5, 
+                   progressBar=FALSE,
+                   cpus=snakemake@threads)
 # plot error rate by number of components used by the model
 pdf(snakemake@output[["ncompBER_plot"]]
     width = 8, height = 5, bg = "white", 
@@ -53,89 +56,109 @@ pdf(snakemake@output[["ncompBER_plot"]]
 plot(perf.diablo) 
 dev.off() 
 
+# tune the model on the train set
 # optimise the number of features for each 'omic, component
-test.keepX=list("expr"=c(seq(5, 30, 5)),
-                "mut"=c(seq(5, 15, 5)),
-               "meth"=c(seq(5, 30, 5)),
-                "cnv"=c(seq(5, 30, 5)))
-
-#optimal_ncomp<-perf.diablo$choice.ncomp$WeightedVote["Overall.BER", "mahalanobis.dist"]
-optimal_ncomp<-3
-tune.omics=tune.block.splsda(X=data,Y=Y,
-                             ncomp=optimal_ncomp,test.keepX=test.keepX,
-                             design=design,cpus=4,progressBar=TRUE,
-                             validation="Mfold",folds=4,nrepeat=10,
+test.keepX=list("expr"=c(seq(5, 30, 10)),
+                "mut"=c(seq(5, ncol(mut), 5)),
+               "meth"=c(seq(5, 30, 10)),
+                "cnv"=c(seq(5, 30, 10)))
+optimal_ncomp<-snakemake@params[["max_ncomp"]]
+tune.omics=tune.block.splsda(X=data,
+                             Y=Y,
+                             ncomp=optimal_ncomp,
+                             test.keepX=test.keepX,
+                             design=design,
+                             cpus=snakemake@threads,
+                             progressBar=FALSE,
+                             validation="Mfold",
+                             folds=4,
+                             nrepeat=50,
                              near.zero.var=FALSE,
                              dist = "mahalanobis.dist")
 
-# final sPLS-DA modelling, display PCA plots and loadings. 
-optimal_ncomp<-tune.omics$choice.ncomp$ncomp
+# train final sPLS-DA model,
+# use optimal number of components, features
+optimal_ncomp<-tune.omics$choice.ncomp$ncomp 
 list.keepX=list("expr"=first(tune.omics$choice.keepX$expr, optimal_ncomp),
                 "mut"=first(tune.omics$choice.keepX$mut, optimal_ncomp),
                 "meth"=first(tune.omics$choice.keepX$meth, optimal_ncomp),
                 "cnv"=first(tune.omics$choice.keepX$cnv, optimal_ncomp))
-
-res=block.splsda(X=data,Y=Y,
+res=block.splsda(X=data,
+                 Y=Y,
                  ncomp=optimal_ncomp,
                  keepX=list.keepX,
                  design=design,
                  near.zero.var=FALSE)
+# save trained model as RDS file
+saveRDS(res, file = snakemake@output[["splsda_model"]])
 
-saveRDS(res, file = "models/DIABLOsplda.rds")
+# assess performance on train set (4-fold CV x 100 repeats) 
+# with Weighted prediction
 perf.res <- perf(res, validation = "Mfold", 
                  folds = 4,  nrepeat = 100,
                  dist = "mahalanobis.dist") 
-
-# Performance on train set (2-fold CV x 100 repeats) with Weighted prediction
-perf.res$WeightedVote.error.rate
+# log balanced error rate on training set
 perf_res_train<-as.data.frame(perf.res$WeightedVote.error.rate)
-write.table(perf_res_train, file = "tables/DIABLOsplda_train_BER.tsv",
-            quote = FALSE, sep = " \t",
-            row.names = TRUE,  col.names = TRUE)
+write.table(perf_res_train, 
+            file = snakemake@output[["trainBER_table"]],
+            quote = FALSE, 
+            sep = "\t",
+            row.names = TRUE,
+            col.names = TRUE)
 
 # Low-dimensional latent PLS space representation of each individual OMIC
 # samples are represented as points placed according to 
 # their projection in the smaller subspace spanned by the components
-pdf("figures/DIABLO_sPLSDA_plotIndiv.pdf",
+pdf(snakemake@output[["PlotIndiv"]],
     width = 10, height = 6, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
 plotIndiv(res,legend=TRUE,
           title="PDx Omics sPLS-DA",
           ellipse=FALSE,
-          ind.names=FALSE,cex=.5,
-          #style = 'graphics'
-          )
+          ind.names=FALSE,
+          cex=.5)
 dev.off() 
 
-# show loadings for each omic and each component
-plotLoadings(res,comp=1,contrib='max',method='median')
-plotLoadings(res,comp=2,contrib='max',method='median')
-plotLoadings(res,comp=3,contrib='max',method='median')
-
-# save mut loadings separately
-res_mut_loadings<-as.data.frame(plotLoadings(res, comp=2, 
-                                             contrib='max',
-                                             method='median', 
-                                             plot=FALSE, 
-                                             block = "mut"))
-write.table(res_mut_loadings, file = "tables/DIABLOsplda_train_mutLoadings.tsv",
-            quote = FALSE, sep = " \t",
-            row.names = TRUE,  col.names = TRUE)
-
+# show and save loadings for each omic and each component
+pdf(snakemake@output[["PlotLoadings"]],
+    width = 10, height = 6, bg = "white", 
+    colormodel = "cmyk",paper = "A4")
+for (c in seq(0, optimal_ncomp))
+{
+    plotLoadings(res,comp=c,contrib='max',method='median')
+}
+dev.off()
+loadings<-as.data.frame(plotLoadings(res,
+                             contrib='max',
+                             method='median',
+                             plot=FALSE)) 
+write.table(loadings, 
+            file = snakemake@output[["loadings_table"]],
+            quote = FALSE,
+            sep = "\t",
+            row.names = TRUE,
+            col.names = TRUE)
 
 # Now we will diplay each individual on the plot of each of the OMICs
-# against each other which are defined by the top loadings from their respective components.
-plotDiablo(res,ncomp=1)
-plotDiablo(res,ncomp=2)
+# against each other which are defined by the top loadings from 
+# their respective components.
+pdf(snakemake@output[["PlotLoadings"]],
+    width = 10, height = 6, bg = "white", 
+    colormodel = "cmyk",paper = "A4")
+for (c in seq(0, optimal_ncomp))
+{
+   plotDiablo(res,ncomp=c) 
+}
+dev.off()
 
-# Now let us display so-called “arrow plot” which demonstrates the samples (individuals) 
-# in a superimposed manner where each sample will be indicated using an arrow. 
+# Now let us display so-called “arrow plot”  
 # The start of the arrow indicates the location of the sample in X in one plot, 
 # and the tip the location of the sample in Y in the other plot. 
 # Short arrows indicate a strong agreement between the matching data sets, 
 # long arrows a disagreement between the matching data sets.
-# Such graphic highlight the agreement between all data sets at the sample level, when modelled with DIABLO.
-pdf("figures/DIABLO_sPLSDA_plotArrow.pdf",
+# This plot highlights the agreement between all data sets at 
+# the sample level, when modelled with DIABLO.
+pdf(snakemake@output[["plotArrow"]],
     width = 8, height = 6, bg = "white",
     colormodel = "cmyk",paper = "A4") 
 plotArrow(res,ind.names=FALSE,legend=TRUE,
@@ -146,59 +169,78 @@ dev.off()
 # correlation among different OMICs datasets
 # the variables for this plot were selected simultaneously from all the OMICs, i.e. they are 
 # not equavalent to those obtained from each individual OMIC separately.
-pdf("figures/DIABLO_sPLSDA_circosPlot.pdf",
+pdf(snakemake@output[["circosPlot"]],
     width = 18, height = 16, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
-circosPlot(res,cutoff=.5,line=FALSE,size.variables=0.5)
+circosPlot(res,
+           cutoff=.5,
+           line=FALSE,
+           size.variables=0.5)
 dev.off()
 
-# Correlation network is another way to demostrate correlations 
-# between top loadings of the OMICs data sets in a pairwise fashion.
-network(res,blocks=c(1,2),cex.node.name=0.6,color.node=c('blue','red2'),breaks=NULL)
-
-
-# Finally the correlation heatmap displays strongly correlated blocks of gene expression, 
-# methylation and clinical variables markers that provide clustering of individuals into females and males.
-pdf("figures/DIABLO_sPLSDA_corrHetMap.pdf",
+# Plot the correlation heatmap showing strongly correlated blocks 
+# of gene expression, methylation, mutation, adn CNV 
+# that provide clustering of individuals into females and males.
+pdf(snakemake@output[["corrHeatmap"]]
     width = 24, height = 14, bg = "white", 
     colormodel = "cmyk",paper = "A4") 
 cimDiablo(res)
 dev.off()
 
-### Predict Cetuximab 3w response (cat) from OMICs Integration,
-# assess the prediction accuracy
+### Predict target classes from OMICs Integration,
+# assess prediction accuracy
 
 # load test data for e/a omic
-f <- "tables/PDx_Expr_MultiFeatSelect_XtestClean.tsv"
+f <- snakemake@input[["expr_Xtest"]]
 expr_test <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(expr_test)<-expr_test$V1
 expr_test<-subset(expr_test, select = -c(V1))
-f <- "tables/PDx_Meth_MultiFeatSelect_XtestClean.tsv"
+f <- snakemake@input[["meth_Xtest"]]
 meth_test <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(meth_test)<-rownames(expr_test)
 meth_test<-subset(meth_test, select = -c(V1))
-f <- "tables/PDx_CNV_FeatSelect_XtestClean.tsv"
+f <- snakemake@input[["cnv_Xtest"]]
 cnv_test <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(cnv_test)<-rownames(expr_test)
 cnv_test<-subset(cnv_test, select = -c(V1))
-f <- "tables/PDx_driverMutVAF_FeatSelect_XtestClean.tsv"
+f <- snakemake@input[["mut_Xtest"]] 
 mut_test <- as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(mut_test)<-rownames(expr_test)
 mut_test<-subset(mut_test, select = -c(V1))
 # load target
-f <- "tables/Cetuximab_3w_cat_testClean.tsv"
+f <- snakemake@input[["Ytest"]] 
 Y_test<-as.data.frame(fread(f, header = TRUE, sep="\t"))
 rownames(Y_test)<-rownames(expr_test)
 Y_test<-subset(Y_test, select = -c(V1))
-Y_test<-factor(Y_test$Cetuximab_Standard_3wks_cat)
+Y_test<-factor(Y_test$snakemake@params[["target_col"]])
 
-# build test design matrix
+# build test set
 data.test<-list(expr=expr_test,mut=mut_test,meth=meth_test,cnv=cnv_test)
 lapply(data.test, dim)
 
-predict.diablo=predict(res,newdata=data.test,dist='mahalanobis.dist')
-predictions<-predict.diablo$WeightedVote$mahalanobis.dist[,1]
+# make predictions
+predict.diablo=predict(res,
+                       newdata=data.test,
+                       dist='mahalanobis.dist')
+
+predictions<-predict.diablo$WeightedVote$mahalanobis.dist[,optimal_ncomp]
 confusion.mat = get.confusion_matrix(truth = Y_test, 
                                      predicted = predictions)
-get.BER(confusion.mat)
-confusion.mat
+# save confusion matrix
+write.table(confusion.mat, 
+            file = snakemake@output[["confusionMat"]],
+            quote = FALSE,
+            sep = "\t",
+            row.names = TRUE,
+            col.names = TRUE) 
+
+for (c in seq(0, optimal_ncomp))
+{
+# save balanced error rate for e/a component
+lapply(c(c, get.BER(confusion.mat)), 
+         write,
+         snakemake@log[[1]], 
+         append=TRUE)
+}
+
+
