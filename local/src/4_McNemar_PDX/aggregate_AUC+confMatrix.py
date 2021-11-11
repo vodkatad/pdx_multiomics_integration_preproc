237a3f86e105c665e4902fb2c69183d1995cac0d
Umberto Perron <umberto.perron@fht.org>
2021-11-11 13:23:56 +0100

    add DIABLO to aggregate model eval

--- local/src/4_McNemar_PDX/aggregate_AUC+confMatrix.py
+++ local/src/4_McNemar_PDX/aggregate_AUC+confMatrix.py
@@ -70,7 +70,7 @@ def evaluate_tripleNeg(XtestFile, YtestFile):
     tripleNeg_y_pred = tripleNeg_df_df.loc[X_test.index]
     tripleNeg_y_pred_readable = [Y_class_dict[y] for y in tripleNeg_y_pred]
     # compute accuracy on test set
-    grid_test_score = 1 - \
+    test_accu = 1 - \
         distance.cityblock(y_test, tripleNeg_y_pred) / len(y_test)
     # compute triple neg confusion matrix
     tripleNeg_confMatrix = confusion_matrix(y_test_readable,
@@ -84,24 +84,51 @@ def evaluate_tripleNeg(XtestFile, YtestFile):
     # compute triple neg AUC
     tripleNeg_y_test_predict_proba = tripleNeg_y_pred
     tripleNeg_AUC = roc_auc_score(y_test, tripleNeg_y_test_predict_proba)
-    return ['KRAS_BRAF_NRAS_triple_neg', grid_test_score,
+    return ['KRAS_BRAF_NRAS_triple_neg', test_accu,
             tripleNeg_AUC] + tripleNeg_confusion_df.tolist()
 
+def evaluate_DIABLO(DIABLOPredFile, YtestFile):
+    DIABLOPred = pd.read_csv(DIABLOPredFile, sep='\t', 
+                        header=None, index_col=0)[1]
+    DIABLOPred_readable = [Y_class_dict[y] for y in DIABLOPred]
+    y_test = pd.read_csv(YtestFile, sep="\t", header=0,
+                         index_col=0)[target_col]
+    y_test_readable = [Y_class_dict[y] for y in y_test] 
+    # compute accuracy on test set
+    test_accu = 1 - \
+        distance.cityblock(y_test, DIABLOPred) / len(y_test)
+    # compute DIABLO confusion matrix
+    confMatrix = confusion_matrix(y_test_readable,
+                                            DIABLOPred_readable,
+                                            labels=['PD', 'SD-OR']) 
+    confMatrix_df = pd.DataFrame(confMatrix,
+                                          columns=['pred_PD', 'pred_SD-OR'],
+                                          index=['true_PD', 'true_SD-OR']).stack()
+    confMatrix_df.index = [
+        '__'.join(ix) for ix in confMatrix_df.index]
+    # compute triple neg AUC
+    predict_proba = DIABLOPred
+    DIABLO_AUC = roc_auc_score(y_test, predict_proba)
+    return ['DIABLO_sPLS-DA', test_accu,
+            DIABLO_AUC] + confMatrix_df.tolist()
 
 tab_arr = []
 split_index = 0
 for tupla in zip(snakemake.input.stacked_models,
                  snakemake.input.rawL1elasticnet_models,
+                 snakemake.input.DIABLOsPLSDA_pred,
                  snakemake.input.X_eng,
                  snakemake.input.X_raw,
                  snakemake.input.Y):
-    stackedFile, elasticFile, XtestFile_eng, XtestFile_raw, YtestFile = tupla
+    stackedFile, elasticFile, DIABLOPredFile, XtestFile_eng, XtestFile_raw, YtestFile = tupla
     tab_arr.append([split_index] + evaluate_model(stackedFile,
                                                   XtestFile_eng, YtestFile, 'stackedCVClassifier'))
     tab_arr.append([split_index] + evaluate_model(elasticFile,
                                                   XtestFile_raw, YtestFile, 'rawL1elasticnet'))
     tab_arr.append([split_index] +
                    evaluate_tripleNeg(XtestFile_eng, YtestFile))
+    tab_arr.append([split_index] +
+                   evaluate_DIABLO(DIABLOPredFile, YtestFile))
     split_index += 1
 out_tab = pd.DataFrame(tab_arr, columns=["split_index", 'model_name',
                                          "grid_test_accu",
